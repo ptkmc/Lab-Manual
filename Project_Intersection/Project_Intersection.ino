a6656f55c7b5c8401f534bbc0fc3f3738b991a6f
MakeXchange <32909691+makexchange@users.noreply.github.com>
2017-11-08 12:30:30 -0600

    Update Project_Intersection.ino

--- Project_Intersection/Project_Intersection.ino
+++ Project_Intersection/Project_Intersection.ino
@@ -1,9 +1,8 @@
-// Traffic Lights
-/* This program controls two traffic lights, each with a red, yellow, and green lamp controlling two different streets in a busy city intersection.
+// Intersection
+/* This program controls two traffic lights, each with a red, yellow, and green 
+lamp controlling two different streets in a busy city intersection.
 
-Only one street in this intersection has a crosswalk for pedestrians. A crosswalk signal shines white when it is safe for pedestrians to cross. It will then flash an orange color indicating the lights are about to change. Once it the lights change and it is no longer safe to walk across the street, the orange cross-walk light ceases to flash and shines steadily until it is safe to cross again.
-
-Pedestrians are also given a cross-walk button they can press to stop traffic and be allowed to cross the intersection safely.*/
+*/
 int NorthStreet[]={13,12,11};
 int SouthStreet[]={10,9,8};
 int CrossWalk[]={7,6,5};
